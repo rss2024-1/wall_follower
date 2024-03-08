cc8c49d3bd1af5d2260d89bbf27f607c3dcab6b2
JohlesaO <johlesa@icloud.com>
2024-03-08 11:34:37 -0500

    new implementation of a stopper for safety ctrl

--- wall_follower/safety_controller.py
+++ wall_follower/safety_controller.py
@@ -17,29 +17,24 @@ class SafetyController(Node):
         self.declare_parameter("stopping_distance", "default")
         self.declare_parameter("map_frame", "default")
 
+        ### NEW ####
+        # added this extra distance checker to 'double-check' if it should actually stop
+        self.declare_parameter("checking_distance", "default")
+        # scales the distance from wall to be less than it actually is to account for movement
+        self.declare_parameter("time_safety_scalar", "default")
+        ############
+
         # Fetch constants from the ROS parameter server
         self.SCAN_TOPIC = self.get_parameter('scan_topic').get_parameter_value().string_value
         self.DRIVE_TOPIC = self.get_parameter('drive_topic').get_parameter_value().string_value
         self.VELOCITY = self.get_parameter('velocity').get_parameter_value().double_value
         self.DESIRED_DISTANCE = self.get_parameter('stopping_distance').get_parameter_value().double_value
         self.MAP_FRAME = self.get_parameter('map_frame').get_parameter_value().string_value
-        
 
-	# TODO: Initialize your publishers and subscribers here
-        
-        # SIMULATION
-        # self.subscription = self.create_subscription(
-        #         LaserScan,
-        #         self.SCAN_TOPIC,
-        #         self.laser_callback,
-        #         10)
-        # self.subscription 
-        # self.publisher = self.create_publisher(
-        #         AckermannDriveStamped,
-        #         self.DRIVE_TOPIC,
-        #         10)
-        
-        # self.line_pub = self.create_publisher(Marker, self.SCAN_TOPIC, 1)
+        ### NEW ###
+        self.CHECKING_DISTANCE = self.get_parameter('checking_distance').get_parameter_value().double_value
+        self.TIME_SAFETY_SCALAR = self.get_parameter('time_safety_scalar').get_parameter_value().double_value
+        ###########
 
         # REAL ROBOT publisher/subscribers
         self.subscription = self.create_subscription(
@@ -52,35 +47,45 @@ class SafetyController(Node):
 
     def laser_callback(self, scan):
 
+        ### NEW ####
+        # calculate some dt
+        time_now = rclpy.time.Time()
+        dt = time_now-time_before
+        time_before = rclpy.time.Time()
+
+        # get checking distance and time scalar for later calculation
+        self.CHECKING_DISTANCE = self.get_parameter('checking_distance').get_parameter_value().double_value
+        self.TIME_SAFETY_SCALAR = self.get_parameter('time_safety_scalar').get_parameter_value().double_value
+        ############
+
+        # get current speed and desired stopping distance
         self.STOPPING_DISTANCE = self.get_parameter('stopping_distance').get_parameter_value().double_value
         self.VELOCITY = self.get_parameter('velocity').get_parameter_value().double_value
 
+        # distance and angle scans
         ranges = np.array(scan.ranges)
-        angles = np.arange(scan.angle_min, scan.angle_max, scan.angle_increment)
         divider_ix = int(len(ranges)/10) 
-
         front_ranges = ranges[divider_ix*4:6*divider_ix]
-        front_angles = angles[divider_ix*4:6*divider_ix]
-        # estimate wall 
+        # angles = np.arange(scan.angle_min, scan.angle_max, scan.angle_increment)
+        # front_angles = angles[divider_ix*4:6*divider_ix]
         
-        close_count = (front_ranges < self.STOPPING_DISTANCE).sum()
+        # speed alteration logic
+        drive_command = AckermannDriveStamped() # message template for drive cmd
 
-        drive_command = AckermannDriveStamped()
-
-        if close_count > 3: 
-            drive_command.drive.speed = 0.0
-        else:
-            drive_command.drive.speed = self.VELOCITY
-
-        drive_command.header.stamp = rclpy.time.Time().to_msg()
-        drive_command.header.frame_id = self.MAP_FRAME
-    
-        drive_command.drive.steering_angle = 0.0
-        drive_command.drive.steering_angle_velocity = 0.0
-    
-        drive_command.drive.acceleration = 0.0
-        drive_command.drive.jerk = 0.0
-
+        #### NEW #######
+        # gives a buffer; checks if current readings pass a 'warning threshold'
+        # stops if we're really going to crash
+        close_points_to_check = [dist for dist in front_ranges if dist < self.STOPPING_DISTANCE]
+        closest_dist = min(close_points_to_check)
+        if closest_dist < self.CHECKING_DISTANCE:
+            predicted_closest_reading = closest_dist - self.VELOCITY*self.TIME_SAFETY_SCALAR*dt
+            if predicted_closest_reading <= self.STOPPING_DISTANCE:
+                drive_command.drive.speed = 0.0
+        ################
+        
+        # close_count = (front_ranges < self.STOPPING_DISTANCE).sum()
+        # if close_count > 3: 
+        #     drive_command.drive.speed = 0.0
         self.safety_publisher.publish(drive_command)
 
 def main():
